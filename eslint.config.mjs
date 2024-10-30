dcfe1bcb2476e8b94cdf5a8b415f63ec32aeb219
Celeste-Rhoades <celesteisthebest82@gmail.com>
2024-10-29 20:26:08 -0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off", //makes it so you can use single quotes and not have to write &apos
+      "react/prop-types": "off",
+    },
+  },
+  prettier,
+];
